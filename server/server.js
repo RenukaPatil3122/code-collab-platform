30c1483484ab35426a2cb7dee1583fc1466cb11e
Renuka Patil <renukapatil627@gmail.com>
2026-03-03 15:55:50 +0530

    add: added patch method in cors

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
